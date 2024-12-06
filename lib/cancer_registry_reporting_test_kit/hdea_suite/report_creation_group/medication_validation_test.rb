32208170e90c3912cd69e6c149140ce72ddd1719
Christine Duong <cqduong@mitre.org>
2024-12-06 10:28:50 -0500

    Added TODOs and resource size enforce

--- lib/cancer_registry_reporting_test_kit/hdea_suite/report_creation_group/medication_validation_test.rb
+++ lib/cancer_registry_reporting_test_kit/hdea_suite/report_creation_group/medication_validation_test.rb
@@ -29,6 +29,9 @@ fail if their code/system are not found in the valueset.
       end
 
       run do
+        # TODO: Check for type due to open slicing. For now, enforce check for only 1 resource
+        assert scratch_resources[:all].length < 2, "Test currently allows only for 1 resource for this type."
+
         perform_validation_test(scratch_resources[:all] || [],
                                 'http://hl7.org/fhir/us/core/StructureDefinition/us-core-medication',
                                 '5.0.1',
