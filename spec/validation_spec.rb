8abf9406185b6a94398ae6f34ae7c3b73c8008c4
360dgries <dgriese@mitre.org>
2025-01-28 12:24:56 -0500

    Fix spec test for multiple reports

--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe CancerRegistryReportingTestKit::HDEAV100::CcrrContentBundleValidationTest do
   let(:suite) { Inferno::Repositories::TestSuites.new.find('ccrr_v100_report_generation') }
   let(:session_data_repo) { Inferno::Repositories::SessionData.new }
+  let(:test_scratch) { {} }
   let(:test_session) { repo_create(:test_session, test_suite_id: suite.id) }
 
   def run(runnable, inputs = {})
@@ -11,7 +12,7 @@ RSpec.describe CancerRegistryReportingTestKit::HDEAV100::CcrrContentBundleValida
         test_session_id: test_session.id,
         name: name,
         value: value,
-        type: 'text' # edited for this kit
+        type: runnable.config.input_type(name)
       )
     end
     Inferno::TestRunner.new(test_session: test_session, test_run: test_run).run(runnable)
@@ -24,12 +25,11 @@ RSpec.describe CancerRegistryReportingTestKit::HDEAV100::CcrrContentBundleValida
     let(:validation_ok_response) { File.read('spec/fixtures/validation_ok.json')}
 
     it 'stores all resources in scratch when given multiple input reports' do
-      # validation_test_instance = validation_test.new TODO: figure out if better to use instance or class -- `run` method needs class, but if instance could access scratch?
+      allow_any_instance_of(validation_test).to receive(:scratch).and_return(test_scratch)
       stub_request(:post, "https://example.com/validatorapi/validate").to_return(status: 200, body: validation_ok_response, headers: { 'Content-Type' => 'application/fhir+json' })
-      result = run(validation_test, {reports: incomplete_bundle})
-      # resources = validation_test_instance.scratch_resources[:all] TODO: fails, even when an instance -- does not seem to be able to find `scratch` from the main session, but can respond to scratch_resources
-      #TODO: Figure out how to access scratch from within rspec
-      #TODO: Access scratch after running validation test to see if resources from both reports were read in -- also need to make two small distinct reports to test this with.
+      result = run(validation_test, {reports: incomplete_bundle + ',' + complete_bundle})
+      patient_ids = test_scratch[:patient_resources][:all].map { |resource| resource.id}
+      expect(patient_ids).to eq(["example-incomplete-Patient", "example-complete-Patient"])
     end
   end
 end
