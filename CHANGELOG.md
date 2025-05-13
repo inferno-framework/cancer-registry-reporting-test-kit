80f4408e81367ef41e6e0addf4e3b781bbf8eee9
rpassas <rpassas@mitre.org>
2025-05-13 09:29:48 -0400

    release 0.9.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
 # 0.9.0
 Initial public release of Cancer Registry Reporting Test Kit in GitHub.
+
+# 0.9.1
+* readme and rm ci by @rpassas in https://github.com/inferno-framework/cancer-registry-reporting-test-kit/pull/2
--- lib/cancer_registry_reporting_test_kit/version.rb
+++ lib/cancer_registry_reporting_test_kit/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module CancerRegistryReportingTestKit
-  VERSION = '0.9.0'
-  LAST_UPDATED = '2025-05-07' # TODO: update next release
+  VERSION = '0.9.1'
+  LAST_UPDATED = '2025-05-13' # TODO: update next release
 end
